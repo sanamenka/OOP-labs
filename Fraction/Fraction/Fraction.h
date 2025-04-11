4e1a5fc980567c978474cdd39a7621aa6a1e3f05
sanamenka <vega_gera@mail.ru>
2025-04-12 00:28:07 +0700

    объявление методов класса

--- Fraction/Fraction/Fraction.h
+++ Fraction/Fraction/Fraction.h
@@ -1,5 +1,36 @@
-#pragma once
-class Fraction
-{
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include <iostream>
+
+class Fraction {
+private:
+
+	int numerator;
+	int detorminator;
+
+public:
+	Fraction();
+	Fraction(int num, int den);
+
+	friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
+	friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
+
+	// Методы получения и установки числ. знам.
+	void setNumenator(int num);
+	void setDenominator();
+	int getNumenator() const;
+	int getNumenator() const;
+
+	// Перегрузка операций
+	Fraction operator+(const Fraction& other) const;
+	Fraction operator-(const Fraction& other) const;
+	Fraction operator*(const Fraction& other) const;
+	Fraction operator/(const Fraction& other) const;
+	bool operator>(const Fraction& other) const;
+	bool operator<(const Fraction& other) const;
+	bool operator==(const Fraction& other) const;
+	bool operator!=(const Fraction& other) const;
 };
 
+#endif
