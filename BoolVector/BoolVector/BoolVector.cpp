6c3fc417009831bca7711895b94e2f5991fc3773
sanamenka <vega_gera@mail.ru>
2025-04-24 15:17:28 +0700

    Объявление всех методов и перегрузок, реализация конструкторов

--- BoolVector/BoolVector/BoolVector.cpp
+++ BoolVector/BoolVector/BoolVector.cpp
@@ -17,3 +17,33 @@ BoolVector::BoolRank& BoolVector::BoolRank::operator=(bool value) {
     else byte &= ~(1 << bit);
     return *this;
 }
+
+BoolVector::BoolVector() : nBits(8), nBytes(1) {
+    data = new unsigned char[nBytes];
+    std::memset(data, 0, nBytes);
+}
+
+BoolVector::BoolVector(int size, bool value) : nBits(size), nBytes((size + 7) / 8) {
+    data = new unsigned char[nBytes];
+    std::memset(data, value ? 0xFF : 0x00, nBytes);
+    if (value && nBits % 8)
+        data[nBytes - 1] &= (1 << (nBits % 8)) - 1;
+}
+
+BoolVector::BoolVector(const char* str) {
+    nBits = std::strlen(str);
+    nBytes = (nBits + 7) / 8;
+    data = new unsigned char[nBytes];
+    std::memset(data, 0, nBytes);
+    for (int i = 0; i < nBits; ++i)
+        if (str[i] == '1') set(i, 1);
+}
+
+BoolVector::BoolVector(const BoolVector& other) : nBits(other.nBits), nBytes(other.nBytes) {
+    data = new unsigned char[nBytes];
+    std::memcpy(data, other.data, nBytes);
+}
+
+BoolVector::~BoolVector() {
+    delete[] data;
+}
--- BoolVector/BoolVector/BoolVector.h
+++ BoolVector/BoolVector/BoolVector.h
@@ -20,6 +20,45 @@ public:
         BoolRank& operator=(bool value);
     };
 
+    BoolVector();
+    BoolVector(int size, bool value = false);
+    BoolVector(const char* str);
+    BoolVector(const BoolVector& other);
 
+    ~BoolVector();
+
+    int get_length() const;
+    int get_weight() const;
+
+    void swap(BoolVector& other);
+
+    void invert();
+    void invert(int i);
+
+    void set(int i, bool value);
+    void set(int i, int k, bool value);
+    void setAll(bool value);
+
+    BoolRank operator[](int i);
+    bool operator[](int i) const;
+
+    BoolVector& operator=(const BoolVector& other);
+
+    BoolVector operator&(const BoolVector& other) const;
+    BoolVector& operator&=(const BoolVector& other);
+    BoolVector operator|(const BoolVector& other) const;
+    BoolVector& operator|=(const BoolVector& other);
+    BoolVector operator^(const BoolVector& other) const;
+    BoolVector& operator^=(const BoolVector& other);
+    BoolVector operator~() const;
+
+    BoolVector operator<<(int shift) const;
+    BoolVector& operator<<=(int shift);
+
+    BoolVector operator>>(int shift) const;
+    BoolVector& operator>>=(int shift);
+
+    friend std::ostream& operator<<(std::ostream& os, const BoolVector& bv);
+    friend std::istream& operator>>(std::istream& is, BoolVector& bv);
 };
 
